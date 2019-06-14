1e90b2fad0d409416a63558820fae286d73976c9
zhang.mingyang <123@qq.com>
2019-06-14 18:18:51 +0800

    contain modify

--- src/views/framework/aside_menu.vue
+++ src/views/framework/aside_menu.vue
@@ -1,40 +1,27 @@
 <template>
   <el-menu
-    default-active="1-4-1"
-    class="el-menu-vertical-demo"
-    background-color="#20222A"
-    text-color="#E6E6E6E6"
-    active-text-color="#2F9688">
-    <el-submenu index="1">
-      <template slot="title">
-        <i class="el-icon-location"></i>
-        <span slot="title">导航一</span>
-      </template>
-      <el-menu-item-group>
-        <span slot="title">分组一</span>
-        <el-menu-item index="1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
-      </el-menu-item-group>
-      <el-menu-item-group title="分组2">
-        <el-menu-item index="1-3">选项3</el-menu-item>
-      </el-menu-item-group>
-      <el-submenu index="1-4">
-        <span slot="title">选项4</span>
-        <el-menu-item index="1-4-1">选项1</el-menu-item>
+    :default-active="activeIndex2"
+    class="el-menu-demo"
+    mode="horizontal"
+    @select="handleSelect"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b">
+    <el-menu-item index="1">处理中心</el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">我的工作台</template>
+      <el-menu-item index="2-1">选项1</el-menu-item>
+      <el-menu-item index="2-2">选项2</el-menu-item>
+      <el-menu-item index="2-3">选项3</el-menu-item>
+      <el-submenu index="2-4">
+        <template slot="title">选项4</template>
+        <el-menu-item index="2-4-1">选项1</el-menu-item>
+        <el-menu-item index="2-4-2">选项2</el-menu-item>
+        <el-menu-item index="2-4-3">选项3</el-menu-item>
       </el-submenu>
     </el-submenu>
-    <el-menu-item index="2">
-      <i class="el-icon-menu"></i>
-      <span slot="title">导航二</span>
-    </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <i class="el-icon-document"></i>
-      <span slot="title">导航三</span>
-    </el-menu-item>
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <span slot="title">导航四</span>
-    </el-menu-item>
+    <el-menu-item index="3" disabled>消息中心</el-menu-item>
+    <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
   </el-menu>
 </template>
 
@@ -43,6 +30,7 @@ export default {
   name: 'aside_menu',
   data () {
     return {
+      activeIndex2: '1'
     }
   }
 }
--- src/views/framework/framework.vue
+++ src/views/framework/framework.vue
@@ -1,11 +1,11 @@
 <template>
   <el-container>
-    <el-aside class="sidebar-container">
+    <el-aside>
       <aside-menu></aside-menu>
     </el-aside>
-    <el-container>
-      <router-view></router-view>
-    </el-container>
+    <!--<el-container>-->
+      <!--<router-view></router-view>-->
+    <!--</el-container>-->
   </el-container>
 </template>
 
@@ -18,11 +18,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.sidebar-container{
-  transition: width 1s;
-  -moz-transition: width 1s;
-  -webkit-transition: width 1s;
-  -o-transition: width 1s;
-  z-index: 99;
-}
+  .sidebar-container{
+    transition: width 1s;
+    -moz-transition: width 1s;
+    -webkit-transition: width 1s;
+    -o-transition: width 1s;
+    z-index: 99;
+  }
+  .el-aside{
+    width: 100%;
+  }
 </style>
